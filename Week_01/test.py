80de01f1b14b6ca9846f551a2e7c0630d642ea6e
lixiawei <13808196383@163.com>
2020-05-18 21:48:26 +0800

    test for delete

--- Week_01/test.py
+++
@@ -1,9 +0,0 @@
-class TestClass():
-    def __init__(self):
-        self._info = "Hello World"
-    def show_info(self):
-        print(self._info)
-
-if __name__ == "__main__":
-    test = TestClass()
-    test.show_info()
